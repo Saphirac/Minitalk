761d525035f1c92c66d1d1ab767ab653e37bef46
Meryem COURTOIS <mcourtoi@e1r10p12.42.fr>
2022-05-12 02:04:28 +0200

    time to tst

--- srcs_client/client.c
+++ srcs_client/client.c
@@ -6,18 +6,35 @@
 /*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 19:49:00 by mcourtoi          #+#    #+#             */
-/*   Updated: 2022/05/12 01:49:36 by mcourtoi         ###   ########.fr       */
+/*   Updated: 2022/05/12 02:01:13 by mcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
 int	ft_check(int ac, char **av)
 {
+	int	i;
+
+	i = 0;
 	if (ac != 3)
 		return (1);
 	if (ft_atoi(av[1]) <= 0)
 		return (1);
+	while (av[1][i])
+	{
+		if (ft_isdigit(av[1][i]) == 0)
+			return (1);
+		i++;
+	}
 	return (0);
 }
 
@@ -58,7 +75,10 @@ void	ft_send_message(int pid, char *msg)
 int	main (int ac, char **av)
 {
 	if (ft_check(ac, av) == 1)
+	{
+		ft_putstr_fd("Error.\n", 2);
 		return (1);
+	}
 	ft_send_message(ft_atoi(av[1]), av[2]);
 	return (0);
 }
